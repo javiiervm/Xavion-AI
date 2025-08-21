ff04407bbbf20b04337dbc2c0137827fedab2dc7
javiiervm <javiervillamart8@gmail.com>
2025-08-21 17:28:02 +0200

    Voice under development...

--- main.py
+++
@@ -1,8 +0,0 @@
-from backend.chat_workflow import start_chat
-
-if __name__ == "__main__":
-    print("Loading Xavion AI, please wait...", flush=True)
-    debug_mode = False
-    chat_finished = False
-    while not chat_finished:
-        chat_finished = start_chat(debug_mode)
